593692a7e8cc725cfde3361c059fb1564c0fa4b4
steven <12992@holbertonstudents.com>
2026-08-10 13:43:14 +0200

    correction

--- challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -2,49 +2,44 @@
 #include <stdlib.h>
 
 /**
- * delete_dnodeint_at_index - Delete a node at a specific index from a list
+ * delete_dnodeint_at_index - Deletes a node at a given index
+ * @head: Pointer to the head pointer of the list
+ * @index: Index of the node to delete
  *
- * @head: A pointer to the first element of a list
- * @index: The index of the node to delete
- *
- * Return: 1 on success, -1 on failure
+ * Return: 1 if succeeded, -1 if failed
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-    dlistint_t *tmp;
-    unsigned int p = 0;
+	dlistint_t *tmp;
+	unsigned int i = 0;
 
-    if (head == NULL || *head == NULL)
-        return (-1);
+	if (head == NULL || *head == NULL)
+		return (-1);
 
-    tmp = *head;
+	tmp = *head;
 
-    /* Parcourt la liste jusqu'à l'index recherché */
-    while (tmp != NULL && p < index)
-    {
-        tmp = tmp->next;
-        p++;
-    }
+	if (index == 0)
+	{
+		*head = tmp->next;
+		if (*head != NULL)
+			(*head)->prev = NULL;
+		free(tmp);
+		return (1);
+	}
 
-    /* L'index dépasse la taille de la liste */
-    if (tmp == NULL)
-        return (-1);
+	while (tmp != NULL && i < index)
+	{
+		tmp = tmp->next;
+		i++;
+	}
 
-    /* Cas 1 : Suppression du premier nœud (index 0) */
-    if (index == 0)
-    {
-        *head = tmp->next;
-        if (*head != NULL)
-            (*head)->prev = NULL;
-    }
-    /* Cas 2 : Suppression au milieu ou à la fin */
-    else
-    {
-        tmp->prev->next = tmp->next;
-        if (tmp->next != NULL)
-            tmp->next->prev = tmp->prev;
-    }
+	if (tmp == NULL)
+		return (-1);
 
-    free(tmp);
-    return (1);
+	tmp->prev->next = tmp->next;
+	if (tmp->next != NULL)
+		tmp->next->prev = tmp->prev;
+
+	free(tmp);
+	return (1);
 }
