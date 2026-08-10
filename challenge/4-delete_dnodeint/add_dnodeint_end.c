186162ea7613c5911b137c87c4b9b80a5ec129b6
steven <12992@holbertonstudents.com>
2026-08-10 14:59:16 +0200

    tab

--- challenge/4-delete_dnodeint/add_dnodeint_end.c
+++ challenge/4-delete_dnodeint/add_dnodeint_end.c
@@ -11,38 +11,38 @@
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-    dlistint_t *new;
-    dlistint_t *l;
+	dlistint_t *new;
+	dlistint_t *l;
 
-    /* Vérification que le pointeur d'adresse n'est pas NULL */
-    if (head == NULL)
-        return (NULL);
+	/* Vérification que le pointeur d'adresse n'est pas NULL */
+	if (head == NULL)
+		return (NULL);
 
-    new = malloc(sizeof(dlistint_t));
-    if (new == NULL)
-    {
-        return (NULL);
-    }
-    new->n = n;
-    new->next = NULL;
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	new->n = n;
+	new->next = NULL;
 
-    /* Cas 1 : La liste est actuellement vide */
-    if (*head == NULL)
-    {
-        new->prev = NULL;
-        *head = new;
-        return (new);
-    }
+	/* Cas 1 : La liste est actuellement vide */
+	if (*head == NULL)
+	{
+		new->prev = NULL;
+		*head = new;
+		return (new);
+	}
 
-    /* Cas 2 : Parcours jusqu'au dernier élément */
-    l = *head;
-    while (l->next != NULL)
-    {
-        l = l->next;
-    }
+	/* Cas 2 : Parcours jusqu'au dernier élément */
+	l = *head;
+	while (l->next != NULL)
+	{
+		l = l->next;
+	}
 
-    l->next = new;
-    new->prev = l;
+	l->next = new;
+	new->prev = l;
 
-    return (new);
+	return (new);
 }
